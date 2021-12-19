13318632ccd09f17e317eb2da046efe9582f2d00
Salim Khalfouni <salimkhalfouni23@gmail.com>
2021-12-19 14:45:43 +0000

    the update

---
+++ users.js
@@ -0,0 +1,17 @@
+let users =[
+    {id:2,
+    name: 'rihem',
+    age:21,
+    favoriteFoods:['riz','spaghetti']},
+
+    {id:3,
+    name: 'nidhal',
+    age:32,
+    favoriteFoods:['cheesenaan','coscous']},
+
+    {id:4,
+     name:'hiba',
+     age:15,
+     favoriteFoods:['hamburger','sushi']}
+]
+module.exports=users
